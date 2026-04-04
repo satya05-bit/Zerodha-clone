6b1669c0b028dc3d144a7c148c60b3ca72cb9fa7
Satya05-bit <sdas78318@gmail.com>
2026-04-04 12:04:09 +0530

    initial commit

--- dashboard/src/components/Apps.js
+++ dashboard/src/components/Apps.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 const Apps = () => {
-  return <h1>Apps</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  )
 };
 
 export default Apps;
