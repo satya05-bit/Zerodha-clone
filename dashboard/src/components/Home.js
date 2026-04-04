9191da1b6a68ec78b07b0356a6ceb4b0aa5c0606
Satya05-bit <sdas78318@gmail.com>
2026-04-04 12:29:03 +0530

    initial commit

--- dashboard/src/components/Home.js
+++ dashboard/src/components/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
-
-import Dashboard from "./Dashboard";
+//import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
+import Apps from "./Apps";
 
 const Home = () => {
   return (
     <>
       <TopBar />
-      <Dashboard />
+      <Apps />
     </>
   );
 };
